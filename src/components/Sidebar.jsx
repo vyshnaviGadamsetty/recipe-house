b762539b0d4258e3553f63a857befbdf8c333ded
Vyshnavi Gadamsetty <vyshnavigadamsetty@gmail.com>
2025-03-29 15:46:12 +0530

    updated recipe house

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,8 +16,8 @@ const DesktopSidebar = () => {
 		<div className='p-3 md:p-10 border-r min-h-screen w-24 md:w-64 hidden sm:block'>
 			<div className='flex flex-col gap-20 sticky top-10 left-0'>
 				<div className='w-full'>
-					<img src='/j.png' alt='logo' className='hidden md:block' />
-					<img src='/mobile-logo.svg' alt='logo' className='block md:hidden' />
+					<img src='/re2.png' alt='logo' className='hidden md:block' />
+					<img src='/re2.png' alt='logo' className='block md:hidden' />
 				</div>
 				<ul className='flex flex-col items-center md:items-start gap-8'>
 					<Link to={"/"} className='flex gap-1'>
